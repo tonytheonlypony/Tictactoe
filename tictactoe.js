7ffb0783b582763cf2f125235e9e4bfcdad3236f
tonytheonlypony <anthonybone@outlook.com>
2021-03-25 23:24:47 -0700

    Square class -> square function done
    taking turns next

--- tictactoe.js
+++ tictactoe.js
@@ -1,14 +1,10 @@
-class Square extends React.Component {  
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-        >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick=
+  {props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -17,6 +13,12 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
     };
+  }
+  
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
   
  renderSquare(i) {
